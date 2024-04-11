f951796f3e985ed2f0ecee4b1fbbb270c537e6be
Bright Anyasi <anyasibright@gmail.com>
2024-04-11 20:53:39 +0100

    Added previous, Next and End Quiz buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,27 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  //HANDLES NEXT QUESTION EVENT WHEN ANSWER IS CLICKED
+  // Handles selection of an answer and navigation to the next question
   const handleNextQuestion = (isCorrect) => {
     setCurrentQuestion(currentQuestion + 1);
     setUserAnswers([...userAnswers, isCorrect]);
+  };
+
+  // Handles navigation to the previous question
+  const handlePreviousQuestion = () => {
+    setCurrentQuestion(currentQuestion - 1);
+    // Remove the last answer from userAnswers array
+    setUserAnswers(userAnswers.slice(0, -1));
+  };
+
+  const handleEndQuiz = () => {
+    // If user hasn't answered the current question, submit the quiz without answering
+    if (
+      currentQuestion === questions.length &&
+      userAnswers.length === currentQuestion
+    ) {
+      setCurrentQuestion(currentQuestion + 1); // Move to the Result component
+    }
   };
 
   //RESETS THE QUIZ
@@ -38,6 +55,20 @@ function App() {
           resetQuiz={resetQuiz}
         />
       )}
+      <div className="buttons-div">
+        {/* Display Previous button if not on the first question and not on the result page */}
+        {currentQuestion > 0 && currentQuestion < questions.length && (
+          <button onClick={handlePreviousQuestion}>Previous</button>
+        )}
+        {/* Display Next button if not on the last question and not on the result page */}
+        {currentQuestion < questions.length - 1 && currentQuestion >= 0 && (
+          <button onClick={handleNextQuestion}>Next</button>
+        )}
+        {/* Display End Quiz button if on last question and not on the result page */}
+        {currentQuestion === questions.length && (
+          <button onClick={resetQuiz}>End Quiz</button>
+        )}
+      </div>
     </div>
   );
 }
